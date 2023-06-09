06173ec153d95f395c8d43c53737f22003e0b760
tanzhijian <i@tanzhijian.org>
2023-06-09 23:49:25 +0800

    basic logic
    basic logic

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/tanzhijian/score-simulator"
       target="_blank"
       title="GitHub"
     />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,39 @@
 <script setup lang="ts">
+// 比赛时间为 90 分钟，场均进球数 为 2.79 所以每分钟进球的概率为 3.1%
+const FULLTIME = 90
+const PROB_PER_MINUTE = 0.031
+const state = ref({
+  homeScore: 0,
+  awayScore: 0,
+  timing: 0,
+})
+const timing = $computed(() => state.value.timing)
 
+function whoScored() {
+  if (Math.random() < PROB_PER_MINUTE) {
+    if (Math.random() < 0.5)
+      state.value.homeScore += 1
+    else
+      state.value.awayScore += 1
+  }
+}
+
+function playGame() {
+  reset()
+  const intervalId = setInterval(() => {
+    whoScored()
+    state.value.timing += 1
+
+    if (state.value.timing >= FULLTIME)
+      clearInterval(intervalId)
+  }, 100)
+}
+
+function reset() {
+  state.value.homeScore = 0
+  state.value.awayScore = 0
+  state.value.timing = 0
+}
 </script>
 
 <template>
@@ -19,36 +53,43 @@
         <div flex items-center justify-center>
           <img w-12 src="https://crests.football-data.org/65.png">
         </div>
-        <div>
-          Man City
-        </div>
+        <div>Man City</div>
       </div>
       <div flex-1>
         <div text="10">
-          0 - 0
+          {{ state.homeScore }} - {{ state.awayScore }}
         </div>
         <div text="5 gray">
-          45
+          {{ timing !== 90 ? timing : 'Full-Time' }}
         </div>
       </div>
       <div flex-1>
         <div flex items-center justify-center>
           <img w-12 src="https://crests.football-data.org/108.png">
         </div>
-        <div>
-          Inter
-        </div>
+        <div>Inter</div>
       </div>
     </div>
     <!-- 按钮 -->
     <div flex="~ gap1" justify-center pt-5>
-      <button text="10" i-carbon-play-filled mr-8 bg-green-600 />
-      <button text="10" i-carbon-renew ml-8 bg-green-600 />
+      <button
+        v-if="timing > 0 && timing < 90"
+        text="10"
+        i-carbon-friendship
+        bg-green-600
+      />
+      <button
+        v-else
+        text="10"
+        i-carbon-play-filled
+        bg-green-600
+        @click="playGame()"
+      />
     </div>
     <!-- 模拟内容 -->
-    <div p5 text="left gray" flex="~ gap1">
+    <!-- <div p5 text="left gray" flex="~ gap1">
       <div i-carbon-circle-dash />
       <div i-carbon-circle-filled />
-    </div>
+    </div> -->
   </div>
 </template>
