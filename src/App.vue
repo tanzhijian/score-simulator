340d48e5b3d4cc64249c487e329fd119d5a0abc9
tanzhijian <i@tanzhijian.org>
2023-06-09 20:02:00 +0800

    layout
    layout
    
    layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <header p="y-5" font-mono text="center gray-700 dark:gray-200">
+    <h1 text="8">
+      Score Simulator
+    </h1>
+  </header>
+  <main font-sans p="x-0 y-0" text="center gray-700 dark:gray-200">
     <RouterView />
     <TheFooter />
   </main>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,54 @@
-<script setup lang="ts" generic="T extends any, O extends any">
-defineOptions({
-  name: 'IndexPage',
-})
+<script setup lang="ts">
 
-const name = ref('')
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+  <div ml-2 mr-2 rd-4 bg-white pb-5 pt-5 dark:bg-hex-121212>
+    <!-- 比赛 -->
+    <div flex items-center justify-center pb-5>
+      <div pr-2>
+        <img w-8 src="https://crests.football-data.org/CL.png">
+      </div>
+      <div text="5">
+        Champions League Final
+      </div>
+    </div>
+    <!-- 队伍信息和比分 -->
+    <div flex>
+      <div flex-1>
+        <div flex items-center justify-center>
+          <img w-12 src="https://crests.football-data.org/65.png">
+        </div>
+        <div>
+          Man City
+        </div>
+      </div>
+      <div flex-1>
+        <div text="10">
+          0 - 0
+        </div>
+        <div text="5 gray">
+          45
+        </div>
+      </div>
+      <div flex-1>
+        <div flex items-center justify-center>
+          <img w-12 src="https://crests.football-data.org/108.png">
+        </div>
+        <div>
+          Inter
+        </div>
+      </div>
+    </div>
+    <!-- 按钮 -->
+    <div flex="~ gap1" justify-center pt-5>
+      <button text="10" i-carbon-play-filled mr-8 bg-green-600 />
+      <button text="10" i-carbon-renew ml-8 bg-green-600 />
+    </div>
+    <!-- 模拟内容 -->
+    <div p5 text="left gray" flex="~ gap1">
+      <div i-carbon-circle-dash />
+      <div i-carbon-circle-filled />
     </div>
   </div>
 </template>
