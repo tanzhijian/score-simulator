70ce99b5235a9c646409e76797d6b43e0815f929
tanzhijian <i@tanzhijian.org>
2023-07-02 14:56:23 +0800

    filter matches by date

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,37 @@
 <script setup lang="ts">
-import matches from '~/data/matches.json'
+import matchesData from '~/data/matches.json'
 
-const dateRange = ['Yesterday', 'Today', 'Tomorrow', '2023-05-30']
+let selectedDate = matchesData[1].date
+const selectedMatches = ref()
+
+function filterMatches(date: string) {
+  for (const dayMatches of matchesData) {
+    if (dayMatches.date === date) {
+      selectedDate = dayMatches.date
+      selectedMatches.value = dayMatches.matches
+    }
+  }
+}
+
+filterMatches(selectedDate)
 </script>
 
 <template>
   <div mb-5 flex bg-white shadow="" dark:bg-hex-2e2a2e>
-    <div v-for="date in dateRange" :key="date" flex-1>
+    <div v-for="dayMatches in matchesData" :key="dayMatches.date" flex-1>
       <button
-        v-if="date === 'Today'"
+        v-if="dayMatches.date === selectedDate"
         pb-5
         pt-5
         style="border-bottom: 2px solid"
+        @click="filterMatches(dayMatches.date)"
       >
-        {{ date }}
+        {{ dayMatches.date }}
       </button>
-      <button v-else pb-5 pt-5 text-gray>
-        {{ date }}
+      <button v-else pb-5 pt-5 text-gray @click="filterMatches(dayMatches.date)">
+        {{ dayMatches.date }}
       </button>
     </div>
   </div>
-  <Match v-for="match in matches" :key="match.id" :match="match" />
+  <Match v-for="match in selectedMatches" :key="match.id" :match="match" />
 </template>
